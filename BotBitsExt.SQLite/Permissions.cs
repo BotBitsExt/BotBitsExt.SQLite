1ce54b6879ed9a3ee8388f5dcab191a6bcd6c932
Tunous <lukus178@gmail.com>
2015-12-31 17:32:23 +0100

    Initial commit

---
+++ BotBitsExt.SQLite/Permissions.cs
@@ -0,0 +1,14 @@
+﻿using BotBits.Permissions;
+using SQLite;
+
+namespace BotBitsExt.SQLite
+{
+    [Table("Permissions")]
+    public class Permissions
+    {
+        public string Username { get; set; }
+        public Group Group { get; set; }
+        public string BanReason { get; set; }
+        public long BanTimeout { get; set; }
+    }
+}
---
+++ BotBitsExt.SQLite/SQLiteDatabasePermissionProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using BotBits.Permissions;
+using JetBrains.Annotations;
+using SQLite;
+
+namespace BotBitsExt.SQLite
+{
+    [UsedImplicitly]
+    public class SqLiteDatabasePermissionProvider : IPermissionProvider
+    {
+        private readonly string databasePath;
+
+        public SqLiteDatabasePermissionProvider(string databasePath)
+        {
+            this.databasePath = databasePath;
+            using (var db = new SQLiteConnection(databasePath))
+            {
+                db.CreateTable<Permissions>();
+            }
+        }
+
+        public void SetDataAsync(string storageName, PermissionData permissionData)
+        {
+            using (var db = new SQLiteConnection(databasePath))
+            {
+                db.InsertOrReplace(new Permissions
+                {
+                    Username = storageName,
+                    Group = permissionData.Group,
+                    BanReason = permissionData.BanReason,
+                    BanTimeout = permissionData.BanTimeout.Ticks
+                });
+            }
+        }
+
+        public void GetDataAsync(string storageName, Action<PermissionData> callback)
+        {
+            using (var db = new SQLiteConnection(databasePath))
+            {
+                var permissions = db.Find<Permissions>(it => it.Username == storageName);
+                callback(permissions != null
+                    ? new PermissionData(
+                        permissions.Group,
+                        permissions.BanReason,
+                        new DateTime(permissions.BanTimeout))
+                    : new PermissionData());
+            }
+        }
+    }
+}
