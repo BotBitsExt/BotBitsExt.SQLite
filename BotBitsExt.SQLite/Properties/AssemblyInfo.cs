a4fb85b2f815e1d914c60684f58e7d4d2fcdf1d3
Tunous <lukus178@gmail.com>
2015-12-31 17:40:15 +0100

    Add description

--- BotBitsExt.SQLite/Properties/AssemblyInfo.cs
+++ BotBitsExt.SQLite/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BotBitsExt.SQLite")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("sqlite-net wrapper and SQLite database permission provider for BotBits.Permission")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tunous")]
 [assembly: AssemblyProduct("BotBitsExt.SQLite")]
